f64fe8364c000d188354d660e48e9f35ec2c688c
James <>
2024-07-30 22:43:59 +0900

    feat: kakao 인증 토큰명과 서비스 인증 토큰명이 동일하여 이름 변경

--- src/main/java/com/alphalabs/connectify/member/member/application/port/in/command/RegisterKakaoCommand.java
+++ src/main/java/com/alphalabs/connectify/member/member/application/port/in/command/RegisterKakaoCommand.java
@@ -1,9 +1,7 @@
 package com.alphalabs.connectify.member.member.application.port.in.command;
 
 import com.alphalabs.connectify.common.SelfValidating;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.Pattern;
 import lombok.EqualsAndHashCode;
 import lombok.Value;
 
@@ -12,10 +10,10 @@ import lombok.Value;
 public class RegisterKakaoCommand extends SelfValidating<RegisterKakaoCommand> {
 
 	@NotEmpty(message = "Access token은 필수 입니다.")
-	private final String accessToken;
+	private final String kakaoAccessToken;
 
 	public RegisterKakaoCommand(String accessToken) {
-		this.accessToken = accessToken;
+		this.kakaoAccessToken = accessToken;
 
 		this.validateSelf();
 	}
